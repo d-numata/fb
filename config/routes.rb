208e4373644df76ead0bca08b03130cc163f3ff9
Numata <dnumata@nttpc.co.jp>
2017-10-16 05:55:54 +0000

    201710161455

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+#  get 'comments/create'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
     registrations: "users/registrations",
@@ -20,7 +22,6 @@ resources :blogs do
   resources :comments
     post :confirm, on: :collection
   end
-end
 
 # 
 #resources :blogs do
