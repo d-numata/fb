5606dce5f70980676548966beb13943b8bf8aa5a
Numata <dnumata@nttpc.co.jp>
2017-11-14 02:42:51 +0000

    201711141142

---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#git remote -v
+#git push origin master
+#cat "https://boiling-falls-31557.herokuapp.com/ | https://git.heroku.com/boiling-falls-31557.git"
+
+NOWTIME=`date "+%Y%m%d%H%M"`
+echo $NOWTIME
+##git add -A
+##git commit -m $NOWSTR
+##git push origin master
+##git push heroku master
+#heroku run rake db:migrate RAILS_ENV=production
+#heroku config:add SECRET_TOKEN="$(bundle exec rake secret)"
