870643bf3ae21e8399e44ebf8e2d1086be912b71
Numata <dnumata@nttpc.co.jp>
2017-11-17 17:29:17 +0900

    201711171729

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,5 @@
 class Tweet < ActiveRecord::Base
     validates :content, presence: true
-    validates :content, length: { in: 1..140 }
     validates :image, presence: true
     
     belongs_to :user
