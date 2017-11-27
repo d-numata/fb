b1d4b8961a8aca619ea3e3dcecbf775fde1f3411
Numata <dnumata@nttpc.co.jp>
2017-11-27 09:55:56 +0900

    201711270955

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,4 +1,5 @@
 class ConversationsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @users = User.all  
     @conversations = Conversation.all
@@ -14,8 +15,6 @@ class ConversationsController < ApplicationController
     redirect_to conversation_messages_path(@conversation)
   end
 end
-
-before_action :authenticate_user!
 
 private
   def conversation_params
